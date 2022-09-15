ea9811f9a97d08278e601fad37074cd5b49f8ee0
mohammalmokdad <112966109+mohammalmokdad@users.noreply.github.com>
2022-09-15 10:45:09 -0600

    completed

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,27 +1,51 @@
 from neuron import h
-from neuron.units import mV, ms
+from neuron.units import um, mV, ms
+import plotly
 import matplotlib.pyplot as plt
 h.load_file("stdrun.hoc")
 
 axon=h.Section(name="axon")
-axon.L=1000
-axon.diam=10
-axon.nseg=10
+axon.L=1001 * um
+axon.diam=1 * um
+axon.nseg=1001
 h.hh.insert(axon)
 
 ic=h.IClamp(axon(0))
-ic.delay = 2*ms
+ic.delay = 1*ms
 ic.dur = 0.1*ms
-ic.amp = 10
+ic.amp = 11
 
-d=h.Vector()
-t=h.Vector().record(h._ref_t)
-v=h.Vector().record(axon(0.5)._ref_v)
+rvp=h.RangeVarPlot('v', axon(0), axon(1))
 
 h.finitialize(-65*mV)
-h.continuerun(30*ms)
 
-plt.plot(t,v)
-plt.xlabel('t (ms)')
-plt.ylabel('V (mV)')
-plt.show()
+my_plot=rvp.plot(plotly, name="t=0", line={'width': 4})
+
+for tstop in [0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,10]:
+    h.continuerun(tstop)
+    my_plot=rvp.plot(my_plot, name=f't={tstop}',line={'width': 4})
+
+my_plot = my_plot.update_layout({
+    "xaxis_title": "position (µm)",
+    "yaxis_title": "membrane potential (mV)",
+    "title":'Potential as a function of position (Ra=35.4)'
+})
+
+my_plot.show()
+
+axon.Ra=100
+h.finitialize(-65*mV)
+
+my_plot1=rvp.plot(plotly, name="t=0", line={'width': 4})
+
+for tstop in [0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,10]:
+    h.continuerun(tstop)
+    my_plot1=rvp.plot(my_plot1, name=f't={tstop}',line={'width': 4})
+
+my_plot1 = my_plot1.update_layout({
+    "xaxis_title": "position (µm)",
+    "yaxis_title": "membrane potential (mV)",
+    "title":'Potential as a function of position (Ra=100)'
+})
+
+my_plot1.show()
